59ff818d8267bdad683241ea55f6e1a055207778
Ivan Chavkun'kin <avanes-ch13@yandex.ru>
2023-09-12 17:39:09 +0700

    21. Merge Two Sorted Lists

--- leetcode/lc_21_Merge_Two_Sorted_Lists.kt
+++ leetcode/lc_21_Merge_Two_Sorted_Lists.kt
@@ -61,3 +61,18 @@ fun main(args: Array<String>) {
 //код работает, но не в ide)))
 
 
+//Угнано с leetcode:
+//class Solution {
+//    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+//        if (list1 == null) return list2
+//        if (list2 == null) return list1
+//
+//        return if (list1.`val` <= list2.`val`) {
+//            list1.next = mergeTwoLists(list1.next, list2)
+//            list1
+//        } else {
+//            list2.next = mergeTwoLists(list1, list2.next)
+//            list2
+//        }
+//    }
+//}
