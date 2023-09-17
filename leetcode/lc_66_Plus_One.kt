836d3562f22cc75fb3385951fab7f8b60e1cce36
Ivan Chavkun'kin <avanes-ch13@yandex.ru>
2023-09-17 13:07:15 +0700

    66. Plus One

--- leetcode/lc_66_Plus_One.kt
+++ leetcode/lc_66_Plus_One.kt
@@ -48,3 +48,16 @@ fun main(args: Array<String>) {
         return res
     }
 }
+
+//подсмотрено leetcode
+//fun plusOne(digits: IntArray): IntArray {
+//    for (i in digits.size - 1 downTo 0) {
+//        digits[i] += 1
+//        if (digits[i] <= 9) return digits
+//        digits[i] = 0
+//    }
+//    val arr = IntArray(digits.size + 1)
+//    arr[0] = 1
+//
+//    return arr
+//}
