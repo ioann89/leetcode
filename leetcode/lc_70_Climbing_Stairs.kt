fcc15ce4d8140199de0fe6a00626f034b2cf393c
Ivan Chavkun'kin <avanes-ch13@yandex.ru>
2023-09-19 13:10:17 +0700

    70. Climbing Stairs

--- leetcode/lc_70_Climbing_Stairs.kt
+++ leetcode/lc_70_Climbing_Stairs.kt
@@ -23,14 +23,22 @@ package leetcode
 
 fun main(args: Array<String>) {
     val n = 45
+
     fun climbStairs(n: Int): Int {
-        if (n==1) return 1
+        if (n == 1) return 1
 
+        var dp = IntArray(n + 1)
+        dp[1] = 1
+        dp[2] = 2
 
-        return 0
+        for (i in 3..n) {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+        return dp[n]
     }
     println(climbStairs(n))
 }
 
 
 //Полноценный разбор задачи: https://www.youtube.com/watch?v=2WKsL1TW5eg
+//Но это видео лучше: https://www.youtube.com/watch?v=UUaMrNOvSqg
